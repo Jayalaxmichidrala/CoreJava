7cc730538e1a143ad269212aaa4e0c94e734eeb4
Jaya <jayatatikonda@Mac.hsd1.ca.comcast.net>
2025-07-10 17:18:54 -0700

    Areaparameter file

---
+++ AreaPerimeterCircle.java
@@ -0,0 +1,25 @@
+package javaProject;
+
+//to print the area (r2) and perimeter(2 r) of a circle
+
+
+public class AreaPerimeterCircle {
+	
+	public static void main (String[]args) {
+		int r=8;
+		
+	//	float π=3.14f;
+		
+		//float area=π*r*r;
+	//	float perimeter=π*d;
+		
+		int area=r*r;
+		int perimeter=2*r;
+		
+		System.out.println("Area of circle is: \n"+area);
+		System.out.println("Perimeter of circle is: \n"+perimeter);
+		
+		
+	}
+
+}

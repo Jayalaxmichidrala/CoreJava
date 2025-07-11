e0236aef8f778f8d578bdd962e646fd7a1660647
Jaya <jayatatikonda@Mac.hsd1.ca.comcast.net>
2025-07-10 17:27:54 -0700

    Asterics commit

---
+++ Asterics_Pattern_2.java
@@ -0,0 +1,39 @@
+package javaProject;
+
+/* below output
+11111
+22222
+33333
+44444
+55555
+*/
+
+public class Asterics_Pattern_2 {
+	
+	public static void main(String[]args) {
+		
+			
+		
+		/*for(int i=5;i>1;i=i-1) {
+				//System.out.println(" * ");
+
+				for(int j=5;j>1;j=j-1) {
+			System.out.print(" * "+i);
+			}
+				System.out.println(" * ");
+			}
+			*/
+			
+			for(int i=1;i<=5;i=i+1) {
+				//System.out.println(" * ");
+
+				for(int j=5;j>=1;j=j-1) {
+			System.out.print(i);
+			}
+				System.out.println("");
+			}
+
+	}
+
+
+}
